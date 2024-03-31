1318086bd0fbbf6a545549cb2640be1c17141890
Karkarov <lepsveridze42@gmail.com>
2024-03-31 16:19:56 +0300

    qwerty1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Создаем объекты, используя разные конструкторы
-        Tester tester1 = new Tester("John", "Doe");
+        Tester tester1 = new Tester("John", "Doe");//
         Tester tester2 = new Tester("Alice", "Smith", 3);
         Tester tester3 = new Tester("Bob", "Johnson", 5, "Intermediate", 50000.0);
 
